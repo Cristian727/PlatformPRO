df8437b82888cf950d412cefaf639b86e20054c9
Cristian727 <cristian.gonzalez7@hotmail.com>
2025-03-13 19:19:30 +0000

    EnemyMovement

--- Assets/Scripts/EnemyMoment.cs
+++ Assets/Scripts/EnemyMoment.cs
@@ -4,14 +4,42 @@ using UnityEngine;
 
 public abstract class EnemyMovement : MonoBehaviour
 {
-
     [SerializeField] protected float speed;
-    Transform playerTransform;
-    //[SerializeField] float minSpeed;   Manu dice que seria una habilidad muy rota, no money
-    //[SerializeField] float maxSpeed;
     [SerializeField] protected float maxAngleSpeed;
     protected float actualmaxAngleSpeed;
     [SerializeField] protected float errorMargin = 0.01f;
 
+    [SerializeField] protected Transform playerTransform; 
+    [SerializeField] protected Transform orbitTarget; 
+    [SerializeField] protected float orbitSpeed = 20f;
 
+    protected Rigidbody rb;
+
+    protected virtual void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+    protected virtual void Update()
+    {
+        OrbitAroundPlayer(); 
+        MoveTowardsOrbitTarget(); 
+    }
+
+  
+    protected void OrbitAroundPlayer()
+    {
+        orbitTarget.RotateAround(playerTransform.position, Vector3.up, orbitSpeed * Time.deltaTime);
+    }
+
+
+    protected void MoveTowardsOrbitTarget()
+    {
+        Vector3 direction = orbitTarget.position - transform.position; 
+        direction.y = 0; 
+
+        Vector3 normalizedDirection = direction.normalized;
+
+        rb.velocity = normalizedDirection * speed;
+    }
 }
